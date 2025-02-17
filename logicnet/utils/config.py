f6d04d768afb27da736585bb186af231b228ef5c
trungthanhnguyen0502 <trungthanhnguyen0502@gmail.com>
2025-02-17 17:07:24 +0700

    remove sat_multiple_choice_math_may_23 dataset

--- logicnet/utils/config.py
+++ logicnet/utils/config.py
@@ -195,7 +195,7 @@ def add_args(cls, parser):
             "--dataset_weight",
             type=str,
             help="The weight of the dataset",
-            default="40,30,20,10",
+            default="60,20,20",
         )
 
     else:
